28cd39f86215d2f910f4232d2599890f2bc33820
Mason Phillips <mjphillips2000@gmail.com>
2020-10-31 06:59:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,7 +6,7 @@ class Dog
     
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
   
   def self.all
@@ -23,7 +23,7 @@ class Dog
     end
   end
   
-  def self.save
+  def save
     @@all << self
   end
 end
